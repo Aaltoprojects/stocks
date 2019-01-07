1f8b2c0420142775cd77bca045a3bad176027e1c
financialgoblin <aaltio.amos@gmail.com>
2019-01-08 00:15:06 +0200

    testailua

--- prices.py
+++ prices.py
@@ -6,9 +6,22 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
+#select the time period of the stock data
 start = dt.datetime(2000,1,1)
 end = dt.datetime(2016,12,31)
 
-df = web.Datareader('TSLA', 'yahoo', start, end)
-print(df.head())
+#save the price data of Tesla from yahoo finance api to a dataframe variable
+df = web.DataReader('TSLA', 'yahoo', start, end)
 
+#create an excel file from the data
+df.to_excel('tsla.xlsx')
+
+#read this excel file to a dataframe
+df = pd.read_excel('tsla.xlsx', parse_dates = True, index_col = 0)
+
+#print(df.head())
+
+
+
+df['Adj Close'].plot()
+plt.show()
