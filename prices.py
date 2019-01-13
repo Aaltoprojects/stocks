e38653949b6d68ef6bf80dcffc71593e20741997
financialgoblin <aaltio.amos@gmail.com>
2019-01-13 20:12:53 +0200

    testailua

--- prices.py
+++ prices.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import time
 import matplotlib.pyplot as plt
+import matplotlib.axes as pltx
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
@@ -20,38 +21,101 @@ filename = 'companylist.xlsx'
 book = xlrd.open_workbook(filename)
 sheet = book.sheet_by_index(0)
 
-def correlation(name):
-	df = web.DataReader(name, 'yahoo', start, end)
+def correlation(df):
+
 	change = abs(df['Adj Close'] - df['Open'])
 	vol = df['Volume']
+
 	return vol.corr(change)
 
 names = []
 ids = []
 
+#save names and ids to variables
 for i in range(3427):
 	names.append(str(sheet.cell_value(i+1, 1)))
 	ids.append(str(sheet.cell_value(i+1, 0)))
-names.append('lisaa')
+
+#original correlations' request script
+'''
 def volumechangecorr(numofcos):
 
 	correlations = []
-
 	#max index 3428
 	for i in range(numofcos):
+
 		if names[i+1] != names[i]:
+
 			try:
 				correlations.append(correlation(ids[i+1]))
 				print(correlation(ids[i+1]))
 				print(names[i+1] + '\n')
 			except KeyError:
 				print('No price data\n')
-		else: print('Bad stock\n')
-	return
 
+		else: print('Bad stock\n')
+
+	return correlations
+
+'''
+
+file = open('dataframes.obj', 'rb')
+datadic = pickle.load(file)
+file.close()
+
+#print(datadic.keys())
+#number of keys in dictionary
+#print(len(datadic.keys()))
+#print(datadic['ZNGA'])
+
+correlations = []
+topcompanies = {}
+
+for i in datadic:
+
+	if str(correlation(datadic[i])) == 'nan':
+		
+		print('poistettu')
+
+	else:
+
+		correlations.append(correlation(datadic[i]))
+
+		if correlation(datadic[i]) >= 0.80:
+
+			topcompanies[str(i)] = correlation(datadic[i])
+
+
+print(len(correlations))
+correlations.sort()
+
+print(correlations[2994:3019])
+
+print('top:\n')
+print(topcompanies)
+
+
+'''
+plt.plot(datadic['AMTX']['Adj Close'])
+plt.plot(datadic['AMTX']['Volume'])
+plt.show()
+'''
+vol = datadic['AMTX']['Volume']
+change = abs(datadic['AMTX']['Adj Close'] - datadic['AMTX']['Open'])
+print(vol.corr(change))
+
+plt.figure()
+plt.scatter(vol, change)
+ax = plt.gca()
+ax.set_xlim([0, 100000])
+ax.set_ylim([-0.05, 0.15])
+plt.show()
 
 #creates dictionary of ids and dataframes
-dict = {}
+#dict = {}
+
+'''
+tässä kirjoitettiin .obj tiedostoon dataframet ja avaimiksi osakelyhenteet
 
 for i in range(3427):
 	if names[i+1] != names[i]:
@@ -65,12 +129,13 @@ for i in range(3427):
 	else: print('Bad stock\n')
 
 #write dataframes to .obj file
-file = open('dataframes.obj', 'wb')
+file = open("NIMI.TYYPPI", 'wb')
 pickle.dump(dict, file)
 file.close()
+'''
 
 #read dataframes from .obj file
-#file2 = open('dataframes.obj', 'rb')
+#file2 = open('NIMI.TYYPPI', 'rb')
 #testi = pickle.load(file2)
 #file2.close()
 
